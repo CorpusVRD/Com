269cf43c243002fdd33e074a44845e1c2bfd4c1c
CorpusVRD <system759@mail.ru>
2024-10-25 19:10:53 +0300

    WorkArray
    v.2

--- com/src/main/java/com/edu/DefaultCustomArrayList.java
+++ com/src/main/java/com/edu/DefaultCustomArrayList.java
@@ -1,38 +1,56 @@
 package com.edu;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class DefaultCustomArrayList<E> implements CustomArrayList<E> {
 
-    private E[] list;
+    private  E[] list;
     private int size = 0;
     public static void main(String[] args) {
 
-          DefaultCustomArrayList<Integer> strings = new DefaultCustomArrayList<>();
-          strings.add(1);
-          strings.remove(0);
-          strings.add(2);
-          strings.add(3);
-          // strings.clear();
-           strings.add(1);
-           for (Integer s : strings){System.out.println(s);}
+        DefaultCustomArrayList<Integer> strings = new DefaultCustomArrayList<>();
+        Iterator<Integer> iterator = strings.iterator();
+        for (int l = 0; l < 11 ; l++){
+            strings.add(l);
+        }
+        strings.remove(1);
+        //strings.clear();
+        while (iterator.hasNext()) {
+            Integer current = iterator.next();
+            if (current.equals(5)) {
+               iterator.remove();
+            }
+        }
+        System.out.println(strings.size + " size");
+         for (Integer s : strings){System.out.print(s + ",");}
     }
+
     public DefaultCustomArrayList() {
-        list = (E[]) new Object[0];
+        list = (E[]) new Object[10];
     }
     @Override
     public boolean add(E element) {
-        E[] temp = list;
-        list = (E[]) new Object[temp.length + 1];
-        System.arraycopy(temp, 0, list, 0, temp.length);
+        if (size == list.length) {
+            E[] temp = list;
+            list = (E[]) new Object[temp.length * 2];
+
+            System.arraycopy(temp, 0, list, 0, temp.length);
+        }
         list[size] = element;
         size++;
         return true;
+
     }
     @Override
     public boolean remove(E element) {
-        for (int i = 0; i < size; i++) {
-            if (list[i].equals(element)) {
-                remove(i);
+         for (int k = 0; k < size; k++) {
+            if (list[k].equals(element)) {
+                // вариант1 //
+                System.arraycopy(list, k + 1, list, k, size - k - 1);
+                // вариант2 //
+               // for (int m = k; m < size - k; m++) {list[k] = list[k + 1];}
+
+                list[--size] = null;
                 return true;
             }
         }
@@ -45,6 +63,9 @@ public class DefaultCustomArrayList<E> implements CustomArrayList<E> {
 
     @Override
     public E get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
         return list[index];
     }
 
@@ -79,16 +100,28 @@ public class DefaultCustomArrayList<E> implements CustomArrayList<E> {
     @Override
     public Iterator<E> iterator() {
         return new Iterator<E>() {
-
             int index = 0;
+            boolean canRemove = false;
             @Override
             public boolean hasNext() {
-                return index < list.length;
+                return index < size;
             }
 
             @Override
             public E next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException(" нет элемента hasNext");
+                }
+                canRemove = true;
                 return list[index++];
+            }
+            @Override
+            public void remove() {
+                if (!canRemove) {
+                    throw new IllegalStateException("не удалить элемент перед next()");
+                }
+                DefaultCustomArrayList.this.remove(--index);
+                canRemove = false;
             }
         };
     }
